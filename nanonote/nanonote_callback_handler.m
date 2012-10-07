9efd1a90b7312e0dab31efd6356114fb3efe0d36
kyak <bas@bmail.ru>
2012-10-07 14:59:29 +0400

    Make External Mode work without manual changes to configuration
    And set up some options for target to make it work out of the box.
    
    And use a generic main template, where we choose which template is to be
    used.

--- nanonote/nanonote_callback_handler.m
+++ nanonote/nanonote_callback_handler.m
@@ -8,8 +8,9 @@ slConfigUISetVal(hDlg, hSrc, 'TargetLang', 'C');
 slConfigUISetEnabled(hDlg, hSrc, 'TargetLang', 0);
 
 % Use our own ert_main.c
-slConfigUISetVal(hDlg, hSrc, 'ERTCustomFileTemplate', 'nanonote_srmain.tlc');
+slConfigUISetVal(hDlg, hSrc, 'ERTCustomFileTemplate', 'nanonote_main.tlc');
 slConfigUISetVal(hDlg, hSrc, 'GenerateSampleERTMain', 'off');
+slConfigUISetEnabled(hDlg, hSrc, 'GenerateSampleERTMain',0);
 
 % Nanonote is Little Endian
 slConfigUISetVal(hDlg, hSrc, 'ProdEndianess', 'LittleEndian');
@@ -17,3 +18,20 @@ slConfigUISetVal(hDlg, hSrc, 'ProdEndianess', 'LittleEndian');
 % The target is model reference compliant
 slConfigUISetVal(hDlg, hSrc, 'ModelReferenceCompliant', 'on');
 slConfigUISetEnabled(hDlg, hSrc, 'ModelReferenceCompliant', false);
+
+% Set the solver type to fixed step
+slConfigUISetVal(hDlg, hSrc, 'SolverType', 'Fixed-Step');
+slConfigUISetVal(hDlg, hSrc, 'Solver', 'FixedStepDiscrete');
+
+% Generate report
+slConfigUISetVal(hDlg, hSrc, 'GenerateReport', 'on');
+slConfigUISetVal(hDlg, hSrc, 'LaunchReport', 'off');
+slConfigUISetVal(hDlg, hSrc, 'GenerateCodeMetricsReport', 'on');
+
+% External mode settings
+slConfigUISetVal(hDlg, hSrc, 'ExtModeMexArgs',...
+	['''',getpref('nanonote','HOSTNAME'),'''',' ',num2str(0)]);
+slConfigUISetVal(hDlg, hSrc, 'ExtModeStaticAlloc','on');
+
+% Portable word sizes for switching between PIL/SIL
+slConfigUISetVal(hDlg, hSrc, 'PortableWordSizes','on');
--- nanonote/nanonote_download.m
+++ nanonote/nanonote_download.m
@@ -6,8 +6,17 @@ disp(['### Downloading ', modelName, ' to Nanonote...']);
 %TODO: parse the PROGRAM_FILE_EXT from target_tools.mk
 if (ischar(makertwObj)) %String 'PIL'
 	outfile = modelName;
+	externalMode = 0;
 else
 	outfile = fullfile(makertwObj.BuildDirectory, [modelName, '.out']);
+	
+	makertwArgs = makertwObj.BuildInfo.BuildArgs;
+	
+	for i=1:length(makertwArgs)
+		if strcmp(makertwArgs(i).DisplayLabel,'EXT_MODE')
+			externalMode = str2double(makertwArgs(i).Value);
+		end
+	end
 end
 
 setenv('NANONOTE_EXE',outfile);
@@ -15,19 +24,16 @@ setenv('NANONOTE_EXE',outfile);
 %setenv('NANONOTE_ARGS',this.ArgString);
 setenv('NANONOTE_ARGS','');
 setenv('NANONOTE_HOSTNAME',getpref('nanonote','HOSTNAME'));
+
+if externalMode
+	disp('### Setting up external mode on Nanonote..');
+	setenv('NANONOTE_ARGS','-w'); %wait for Simulink to connect in external mode
+	%disp('Killing previous external mode executables on Nanonote..');
+	!ssh $NANONOTE_HOSTNAME killall `basename $NANONOTE_EXE`
+end
+
 !scp $NANONOTE_EXE $NANONOTE_HOSTNAME:/root
 
-makertwArgs = makertwObj.BuildInfo.BuildArgs;
-externalMode = 0;
-for i=1:length(makertwArgs)
-	if strcmp(makertwArgs(i).DisplayLabel,'EXT_MODE')
-		externalMode = str2double(makertwArgs(i).Value);
-	end
-end
-if externalMode
-	setenv('NANONOTE_ARGS','-w'); %wait for Simulink to connect in external mode
-	disp('Setting up external mode on Nanonote..');
-end
 %need to catch pid below to stop it later..
 disp('Starting executable on Nanonote..');
 !ssh -f $NANONOTE_HOSTNAME /root/`basename $NANONOTE_EXE` $NANONOTE_ARGS
