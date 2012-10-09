94071bc152007c9d6925632fc7301fa4acbc8685
kyak <bas@bmail.ru>
2012-10-09 22:30:42 +0400

    Update setup, add TargetRoot

--- nanonote/nanonote_setup.m
+++ nanonote/nanonote_setup.m
@@ -6,7 +6,10 @@ addpath(fullfile(tgtpath, 'nanonote'));
 addpath(fullfile(tgtpath, 'demos'));
 savepath;
 disp('Ben NanoNote Target Path Setup Complete.');
-%TODO check if pref already exists
-addpref('nanonote','TOOLCHAIN_PREFIX','/home/user/build/openwrt-xburst/staging_dir/toolchain-mipsel_gcc-4.6-linaro_uClibc-0.9.33/bin/mipsel-openwrt-linux-');
-addpref('nanonote','STAGING_DIR','/home/user/build/openwrt-xburst/staging_dir/toolchain-mipsel_gcc-4.6-linaro_uClibc-0.9.33');
+if ispref('nanonote')
+	rmpref('nanonote');
+end
+addpref('nanonote','TOOLCHAIN_PREFIX','/home/user/build/OpenWrt-Toolchain-xburst-for-mipsel-gcc-4.6-linaro_uClibc-0.9.33.2/toolchain-mipsel_gcc-4.6-linaro_uClibc-0.9.33.2/bin/mipsel-openwrt-linux-');
+addpref('nanonote','STAGING_DIR','/home/user/build/OpenWrt-Toolchain-xburst-for-mipsel-gcc-4.6-linaro_uClibc-0.9.33.2/toolchain-mipsel_gcc-4.6-linaro_uClibc-0.9.33.2');
 addpref('nanonote','HOSTNAME','ben');
+addpref('nanonote','TargetRoot',curpath);
